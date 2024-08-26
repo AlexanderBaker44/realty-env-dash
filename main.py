b0b485b20ffcdb578786e26dbe27cc3b8e7bbca2
Alexander Baker <alex_b443@yahoo.com>
2024-08-26 14:24:23 -0400

    dash init

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('data/m_and_e_stacked.csv')
+market_selection = df['MSA/Submarket']
+market_list =list(set([str(i).split('_')[0] for i in market_selection]))
+market_selection = st.selectbox('Select Market', market_list)
+filtered_df = df[df['MSA/Submarket'].str.contains(market_selection)]
+st.table(filtered_df)
